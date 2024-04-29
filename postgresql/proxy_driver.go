bff59364589c734b85f12f0e8a79ad91efe53410
Dave Lambley <dave@lambley.me.uk>
2024-04-29 18:27:09 +0100

    Support "hostaddr" for the postgres scheme
    It is useful to be able to override DNS when connecting to your database.
    libpq supports this,
    
    https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNECT-HOSTADDR
    
    however Go's pq library does not,
    https://github.com/lib/pq/issues/860
    
    We thus implement this with our own dialler, as suggested on the above issue.

--- postgresql/proxy_driver.go
+++ postgresql/proxy_driver.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"database/sql/driver"
+	"errors"
+	"fmt"
 	"net"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/lib/pq"
@@ -19,15 +23,62 @@ func (d proxyDriver) Open(name string) (driver.Conn, error) {
 	return pq.DialOpen(d, name)
 }
 
+func (d proxyDriver) dialWithContext(ctx context.Context, network, address string) (net.Conn, error) {
+	u, err := url.Parse(address)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var port = "5432"
+	if u.Port() != "" {
+		port = u.Port()
+	}
+
+	// https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-PARAMKEYWORDS
+	values, err := url.ParseQuery(u.RawQuery)
+	if err == nil {
+		return nil, err
+	}
+
+	if values.Get("port") != "" {
+		port = values.Get("port")
+	}
+
+	hosts := []string{}
+
+	if values.Get("hostaddr") != "" {
+		hosts = strings.Split(",", values.Get("hostaddr"))
+	}
+
+	if len(hosts) == 0 {
+		hosts = []string{u.Hostname()}
+	}
+
+	for _, host := range hosts {
+		c, e := proxy.Dial(ctx, network, fmt.Sprintf("%s:%s", host, port))
+		if e == nil {
+			return c, e
+		}
+		err = errors.Join(err, e)
+
+		// Report actual address if hostaddr used
+		if values.Get("hostaddr") != "" {
+			err = errors.Join(err, fmt.Errorf("could not connect to %s:%s: %s", host, port, e))
+		}
+	}
+	return nil, err
+}
+
 func (d proxyDriver) Dial(network, address string) (net.Conn, error) {
-	dialer := proxy.FromEnvironment()
-	return dialer.Dial(network, address)
+	return d.dialWithContext(context.TODO(), network, address)
 }
 
 func (d proxyDriver) DialTimeout(network, address string, timeout time.Duration) (net.Conn, error) {
 	ctx, cancel := context.WithTimeout(context.TODO(), timeout)
 	defer cancel()
-	return proxy.Dial(ctx, network, address)
+
+	return d.dialWithContext(ctx, network, address)
 }
 
 func init() {
