36d89617d98e2cc2aef16d3b89543959841e4f06
Dave Lambley <dave@lambley.me.uk>
2024-05-08 12:13:46 +0100

    Show full connection string on error

--- postgresql/config.go
+++ postgresql/config.go
@@ -292,7 +292,7 @@ func (c *Client) Connect() (*DBConnection, error) {
 		}
 		if err != nil {
 			errString := strings.Replace(err.Error(), c.config.Password, "XXXX", 2)
-			return nil, fmt.Errorf("Error connecting to PostgreSQL server %s (scheme: %s): %s", c.config.Host, c.config.Scheme, errString)
+			return nil, fmt.Errorf("Error connecting to PostgreSQL server %s (scheme: %s): %s", dsn, c.config.Scheme, errString)
 		}
 
 		// We don't want to retain connection
