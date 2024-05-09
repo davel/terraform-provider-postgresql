bedb4edda15d21cc9c2d413a08839b6453494f46
Dave Lambley <dave@lambley.me.uk>
2024-05-09 13:43:18 +0100

    Our type?

--- postgresql/config.go
+++ postgresql/config.go
@@ -307,7 +307,7 @@ func (c *Client) Connect() (*DBConnection, error) {
 		var err error
 		if c.config.Scheme == "postgres" {
 			if c.config.HostAddr != "" {
-				var dbConn sql.driver.Conn
+				var dbConn sql.Conn
 				dbConn, err = pq.DialOpen(&driverWrapper{c.config.HostAddr}, dsn)
 				if err == nil {
 					db = sql.OpenDB(dbConn)
