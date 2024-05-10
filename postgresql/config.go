3c1861494998702dc4e7292c06aac68b3c1fc0f8
Dave Lambley <dave@lambley.me.uk>
2024-05-10 15:03:01 +0100

    Fix up formatting

--- postgresql/config.go
+++ postgresql/config.go
@@ -294,7 +294,7 @@ func (c *Client) Connect() (*DBConnection, error) {
 		}
 		if err != nil {
 			errString := strings.Replace(err.Error(), c.config.Password, "XXXX", 2)
-                        dsnString := strings.Replace(dsn, c.config.Password, "XXXX", 2)
+			dsnString := strings.Replace(dsn, c.config.Password, "XXXX", 2)
 			return nil, fmt.Errorf("Error connecting to PostgreSQL server %s (scheme: %s): %s", dsnString, c.config.Scheme, errString)
 		}
 
